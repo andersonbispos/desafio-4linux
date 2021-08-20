7ea88c831f5bcc1a540dcc337a227235927507f0
andersonbispos <andersonbispos@gmail.com>
2021-08-19 22:02:04 -0300

    esconder portas do cadvisor e node-exporter do server

--- scripts/install-monitoramento.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" | tee /etc/apt/sources.list
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-sudo apt update 
-sudo apt install ansible -y
